51b76afac6bed2ea83e18c04defc15c7868c9214
Thejas <thejasrao262003@gmail.com>
2023-07-31 07:23:48 +0530

    Updates

---
+++ precache-manifest.58a26779bc2af4c0ff826ef50d59c8f1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb852a34ccdaf794933b",
+    "url": "/personal_website/static/css/main.4d07d7dd.chunk.css"
+  },
+  {
+    "revision": "cb852a34ccdaf794933b",
+    "url": "/personal_website/static/js/main.4c4686b9.chunk.js"
+  },
+  {
+    "revision": "9c7ca45164327b004de3",
+    "url": "/personal_website/static/js/runtime~main.5454f9ef.js"
+  },
+  {
+    "revision": "fdc8983452f48ab7d3e4",
+    "url": "/personal_website/static/js/2.63cd316f.chunk.js"
+  },
+  {
+    "revision": "e52ed3145c1baf28167af76aa08522b7",
+    "url": "/personal_website/static/media/cover.e52ed314.jpg"
+  },
+  {
+    "revision": "2dd66254db4b0669a1b26e84041584ac",
+    "url": "/personal_website/static/media/project1.2dd66254.png"
+  },
+  {
+    "revision": "c38894f00484ab7c601cc45c8cd7aa73",
+    "url": "/personal_website/static/media/project2.c38894f0.png"
+  },
+  {
+    "revision": "2e92394d96976f7840ef29a3b03870d5",
+    "url": "/personal_website/static/media/project3.2e92394d.PNG"
+  },
+  {
+    "revision": "97ec0a69fb4d475f49ac201f3eea1ce0",
+    "url": "/personal_website/static/media/project4.97ec0a69.PNG"
+  },
+  {
+    "revision": "c15720dcc4f0cab5aa5526080d97cc3e",
+    "url": "/personal_website/static/media/project5.c15720dc.PNG"
+  },
+  {
+    "revision": "0c524ce99beb5fee41a7361d347cc360",
+    "url": "/personal_website/static/media/project6.0c524ce9.PNG"
+  },
+  {
+    "revision": "5ef1e7f3c443369db6b4c8a740228614",
+    "url": "/personal_website/static/media/project7.5ef1e7f3.PNG"
+  },
+  {
+    "revision": "ac658760169ea1e0e4f0c21c895cec0d",
+    "url": "/personal_website/static/media/machinelearning.ac658760.jpg"
+  },
+  {
+    "revision": "1e160539292dc55c052e9fe5936978a9",
+    "url": "/personal_website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/personal_website/precache-manifest.58a26779bc2af4c0ff826ef50d59c8f1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/personal_website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
